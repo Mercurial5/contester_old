f1d8601ad7030a358595f7d04c6d8287aa7cb684
Dias <dias.nespayev@gmail.com>
2021-10-31 13:29:11 +0600

    feat: add samples.db to app

--- contester/problems/__init__.py
+++ contester/problems/__init__.py
@@ -1,4 +1,4 @@
 from contester.problems.routes import app as problems_app
 from contester.problems.models import setup_problems_db
 from contester.problems.models import setup_samples_db
-
+from contester.problems.models import setup_attempts_db
