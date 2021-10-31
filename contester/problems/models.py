04dfc27c58ae2bf13d47ec7c5d9e6cac60ceca9f
Dias <dias.nespayev@gmail.com>
2021-10-31 12:59:01 +0600

    feat: add Attempts model

--- contester/problems/models.py
+++ contester/problems/models.py
@@ -36,4 +36,24 @@ def setup_samples_db(db: SQLAlchemy):
         def get_samples(self, problem_id: int) -> List[Samples]:
             return Samples.query.filter(Samples.problem_id == problem_id).all()
 
+        def get_first_n_samples(self, problem_id: int, count: int) -> List[Samples]:
+            return Samples.query.filter(Samples.problem_id == problem_id).limit(count).all()
+
     return SamplesDBO()
+
+
+def setup_attempts_db(db: SQLAlchemy):
+    class Attempts(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, nullable=False, index=True)
+        problem_id = db.Column(db.Integer, nullable=False, index=True)
+        status = db.Column(db.Boolean, nullable=False)
+        error = db.Column(db.String(255), nullable=True)
+        log = db.Column(db.Text, nullable=True)
+        accepted = db.Column(db.Boolean, nullable=False)
+        failed_case = db.Column(db.Integer, nullable=True)
+
+    class AttemptsDBO(object):
+        pass
+
+    return AttemptsDBO()
