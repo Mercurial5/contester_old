058539fe3b6256bc9dfa6285b18f14e77e384374
Dias <dias.nespayev@gmail.com>
2021-10-31 13:29:58 +0600

    feat: handle user attempts

--- contester/problems/routes.py
+++ contester/problems/routes.py
@@ -34,6 +34,19 @@ def problem_page(problem_id):
 
     samples = samples_db.get_first_n_samples(problem_id, problem.samples_count)
 
-    payload = json.dumps({'id': problem.id, 'host': request.host})
+    payload = json.dumps({'problem_id': problem.id, 'host': request.host})
 
     return render_template('problems/problem_page.html', problem=problem, user=user, samples=samples, payload=payload)
+
+
+@app.route('/save_attempt', methods=['POST'])
+def save_attempt():
+    session = current_app.config['session']
+    attempts_db = current_app.config['attempts.db']
+    post_data = json.loads(request.data.decode('utf-8'))
+
+    user_id = session['user']['id']
+    post_data['accepted'] = False if post_data['status'] is False else post_data.get('accepted')
+
+    attempts_db.save_new_attempt(user_id, post_data)
+    return {}
