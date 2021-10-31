eb7199944c78ec817f2d8ddcaf03d2cc0143ad0a
Dias <dias.nespayev@gmail.com>
2021-10-31 13:35:33 +0600

    feat: add method to fetch attempt counts

--- contester/problems/models.py
+++ contester/problems/models.py
@@ -76,4 +76,7 @@ def setup_attempts_db(db: SQLAlchemy):
         def get_attempts(self, user_id: int, problem_id: int):
             return Attempts.query.filter(Attempts.user_id == user_id, Attempts.problem_id == problem_id).limit(10).all()
 
+        def get_attempts_count(self, user_id: int, problem_id: int):
+            return Attempts.query.filter(Attempts.user_id == user_id, Attempts.problem_id == problem_id).count()
+
     return AttemptsDBO()
